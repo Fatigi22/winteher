bf719445b2f1bd8093584b55fa623c362f6d497b
fatigi22 <fatihiayoubaa22@gmail.com>
2025-01-26 21:20:39 +0100

    ajouter et supremer le favoris

--- app.js
+++ app.js
@@ -9,37 +9,65 @@ let clouds = document.getElementById('clouds');
 let humidity = document.getElementById('humidity');
 let pressure = document.getElementById('pressure');
 let main = document.querySelector('main');
+let favoritesList = document.querySelector('.favorites-list'); // Element to display favorites
+let favorites = []; // Array to hold favorite cities
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();  
     if(valueSearch.value != ''){
         searchWeather();
     }
 });
+
 const searchWeather = () => {
-    fetch(url+'&q='+ valueSearch.value)
+    fetch(url + '&q=' + valueSearch.value)
         .then(response => response.json())
         .then(data => {
             console.log(data);
             if(data.cod == 200){
-                city.querySelector('figcaption').innerHTML = data.name;
-                city.querySelector('img').src = `https://flagsapi.com/${data.sys.country}/shiny/32.png`;
-                temperature.querySelector('img').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
-                temperature.querySelector('span').innerText = data.main.temp;
-                description.innerText = data.weather[0].description;
-
-                clouds.innerText = data.clouds.all;
-                humidity.innerText = data.main.humidity;
-                pressure.innerText = data.main.pressure;
-            }else{
+                displayWeather(data);
+                addToFavorites(data.name); // Call to add to favorites
+            } else {
                 main.classList.add('error');
                 setTimeout(() => {
                     main.classList.remove('error');
                 }, 1000);
             }
             valueSearch.value = '';
-        })
+        });
 }
-// search Default
+
+const displayWeather = (data) => {
+    city.querySelector('figcaption').innerHTML = data.name;
+    city.querySelector('img').src = `https://flagsapi.com/${data.sys.country}/shiny/32.png`;
+    temperature.querySelector('img').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
+    temperature.querySelector('span').innerText = data.main.temp;
+    description.innerText = data.weather[0].description;
+
+    clouds.innerText = data.clouds.all;
+    humidity.innerText = data.main.humidity;
+    pressure.innerText = data.main.pressure;
+}
+
+const addToFavorites = (cityName) => {
+    if (!favorites.includes(cityName)) {
+        favorites.push(cityName);
+        updateFavoritesDisplay();
+    } else {
+        alert(`${cityName} is already in favorites`);
+    }
+}
+
+const updateFavoritesDisplay = () => {
+    favoritesList.innerHTML = ''; // Clear the current list
+    favorites.forEach(city => {
+        let li = document.createElement('li');
+        li.innerText = city;
+        favoritesList.appendChild(li);
+    });
+}
+
+// Search Default
 const initApp = () => {
     valueSearch.value = 'Washington';
     searchWeather();
